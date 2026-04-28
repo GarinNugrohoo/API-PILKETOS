6b6c631b41200f48ee7e722130067404925f5789
GarinNugrohoo <garinnugroho1345@gmail.com>
2026-04-28 09:30:21 +0700

    refactor: update API key env and use x-api-key header for consistency

--- middlewares/apiKey.js
+++ middlewares/apiKey.js
@@ -1,11 +1,11 @@
 class ApiKey {
   keyAccess(req, res, next) {
-    const userApiKey = req.headers["key"];
-    const validApiKey = process.env.KEY;
+    const userApiKey = req.headers["x-api-key"];
+    const validApiKey = process.env.APIKEY;
 
     if (!userApiKey || userApiKey !== validApiKey) {
       return res.status(401).json({
-        message: "API-KEY tidak valid",
+        message: "Invalid APIKEY",
       });
     }
 
