4187bf0523005ca409c1c75b8404032966279fdf
GarinNugrohoo <garinnugroho1345@gmail.com>
2026-05-03 16:56:26 +0700

    fix: add LogVote model import in participantController

--- controllers/participantController.js
+++ controllers/participantController.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const HttpCode = require("./http-code/httpCode");
 const jwt = require("jsonwebtoken");
-const { Participant, sequelize } = require("../models");
+const { Participant, LogVote, sequelize } = require("../models");
 const { Sequelize } = require("sequelize");
 const kandidatController = require("./kandidatController");
 
